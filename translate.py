505ff39169678365c68bb69dd0c59ffb87c5ab13
colddog09 <colddog09@gmail.com>
2026-01-14 03:14:55 +0900

    Add files via upload

---
+++ translate.py
@@ -0,0 +1,6 @@
+translate = {
+    "cane": "개", "cavallo": "말", "elefante": "코끼리", "farfalla": "나비", "gallina": "닭", 
+    "gatto": "고양이", "mucca": "소", "pecora": "양", "ragno": "거미", "scoiattolo": "다람쥐",
+    "dog": "개", "horse": "말", "elephant": "코끼리", "butterfly": "나비", "chicken": "닭", 
+    "cat": "고양이", "cow": "소", "sheep": "양", "spider": "거미", "squirrel": "다람쥐"
+}
